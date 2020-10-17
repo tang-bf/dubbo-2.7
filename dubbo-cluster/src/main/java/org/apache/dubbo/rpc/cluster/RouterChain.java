23669731e4042ab10a639995d9aad0c540f0bc14
tang-bf <1535710733@qq.com>
2020-10-17 23:53:59 +0800

    dubbo 服务引入

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/RouterChain.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/RouterChain.java
@@ -47,6 +47,20 @@ public class RouterChain<T> {
     }
 
     private RouterChain(URL url) {
+        /**
+         * tagrouterfactory
+         * app   dubbo/config/dubbo/dubbo-demo-consumer-application.condition-router
+         * service dubbo/config/dubbo/org.apache.demoservice:1.1.1:g1.condition-router
+         * 根据routerfactory根据url生成各个类型的router
+         * 这里产生的已经可用了  对象内部已经有真实数据了  消费者启动，构造路由连的时候绑定监听器 手动先获取哦数据
+         * 单对于标签路由没有  暂时还相当于没有数据的路由  还没有从配置中心获取数据
+         * 引入服务，获得服务提供者的url时候 ，才会去监听tag-router节点的内容
+         * /dubbo/config/dubbo/dubbo-demo-provider-application.tag-router
+         * 1.request.tag=red 时优先选择 tag=red 的 provider。若集群中不存在与请求标记对应的服务，可以降级请求 tag=null 的 provider，即默认 provider。
+         * 2.request.tag=null 时，只会匹配 tag=null 的 provider。即使集群中存在可用的服务，
+         * 若 tag 不匹配就无法调用，这与规则1不同，携带标签的请求可以降级访问到无标签的服务，
+         * 但不携带标签/携带其他种类标签的请求永远无法访问到其他标签的服务。
+         */
         List<RouterFactory> extensionFactories = ExtensionLoader.getExtensionLoader(RouterFactory.class)
                 .getActivateExtension(url, "router");
 
--- dubbo-demo-tbh/dubbo-demo-provider/src/main/java/com/tbh/provider/DubboProviderDemo.java
+++ dubbo-demo-tbh/dubbo-demo-provider/src/main/java/com/tbh/provider/DubboProviderDemo.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
 public class DubboProviderDemo {
-
+    /**
+     * https://www.jianshu.com/p/8639e5e9fba6
+     * Spring可扩展的XML Schema机制
+     * https://www.jianshu.com/p/bd34e6ae06cf  rpc原理
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderDemo.class,args);
     }
